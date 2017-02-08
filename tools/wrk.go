cf9307d9b5a821f3d561db664158b9816eb9306e
Evgeny Ukhanov <mrlsd@ya.ru>
2017-02-08 12:51:33 +0200

    Added Wrk totol results calculation and print

--- tools/wrk.go
+++ tools/wrk.go
@@ -173,3 +173,113 @@ func (wrk WrkResults) Parse(data []byte) (Results, error) {
 
 	return result, err
 }
+
+// Calculate - mean value of fields
+func (wrk WrkResults) Calculate(data *WrkResults) WrkResults {
+	count := float64(config.Cfg.Try)
+	result := WrkResults{
+		FailedRequests: wrk.FailedRequests/count + data.FailedRequests,
+		ReqSec:         wrk.ReqSec/count + data.ReqSec,
+		Requests:       wrk.Requests/count + data.Requests,
+	}
+
+	result.LatencyStats.Avg = checkTimeQuantor(data.LatencyStats.Avg, wrk.LatencyStats.Avg)
+	result.LatencyStats.Stdev = checkTimeQuantor(data.LatencyStats.Stdev, wrk.LatencyStats.Stdev)
+	result.LatencyStats.Max = checkTimeQuantor(data.LatencyStats.Max, wrk.LatencyStats.Max)
+
+	result.RecSecStats.Avg = checkTransferQuantor(data.RecSecStats.Avg, wrk.RecSecStats.Avg)
+	result.RecSecStats.Stdev = checkTransferQuantor(data.RecSecStats.Stdev, wrk.RecSecStats.Stdev)
+	result.RecSecStats.Max = checkTransferQuantor(data.RecSecStats.Max, wrk.RecSecStats.Max)
+
+	result.LatencyDistribution99pers = checkTimeQuantor(data.LatencyDistribution99pers, wrk.LatencyDistribution99pers)
+
+	return result
+}
+
+// PrintResults - print fields
+func (wrk WrkResults) PrintResults() {
+	fmt.Printf("\tLatency Stats Avg:\t%v %v\n", wrk.LatencyStats.Avg.Time, wrk.LatencyStats.Avg.Quantor)
+	fmt.Printf("\tLatency Stats Stdev:\t%v %v\n", wrk.LatencyStats.Stdev.Time, wrk.LatencyStats.Stdev.Quantor)
+	fmt.Printf("\tLatency Stats Max:\t%v %v\n", wrk.LatencyStats.Max.Time, wrk.LatencyStats.Max.Quantor)
+
+	fmt.Printf("\tReq/Sec Stats Avg:\t%v %v\n", wrk.RecSecStats.Avg.Transfer, wrk.RecSecStats.Avg.Quantor)
+	fmt.Printf("\tReq/Sec Stats Stdev:\t%v %v\n", wrk.RecSecStats.Stdev.Transfer, wrk.RecSecStats.Stdev.Quantor)
+	fmt.Printf("\tReq/Sec Stats Max:\t%v %v\n", wrk.RecSecStats.Max.Transfer, wrk.RecSecStats.Max.Quantor)
+
+	fmt.Printf("\tLatency Distribution\n\t\t\t[99%%]:\t%v %v\n", wrk.LatencyDistribution99pers.Time, wrk.LatencyDistribution99pers.Quantor)
+	fmt.Printf("\tRequests/sec:\t\t%v\n", wrk.ReqSec)
+	fmt.Printf("\tRequests:\t\t%v\n", wrk.Requests)
+	fmt.Printf("\tFailed Requests:\t%v\n", wrk.FailedRequests)
+}
+
+// checkerTimeQuantor - type for checkTimeQuantor
+type checkerTimeQuantor struct {
+	Time    float64
+	Quantor string
+}
+
+// checkQuantor - check is quantor increase/decrease.
+func checkTimeQuantor(from, to checkerTimeQuantor) checkerTimeQuantor {
+	coeff := map[string]float64{
+		"us": 1000000,
+		"ms": 1000,
+		"s":  1,
+	}
+	count := float64(config.Cfg.Try)
+	if from.Quantor == to.Quantor {
+		return checkerTimeQuantor{
+			Time:    to.Time/count + from.Time,
+			Quantor: to.Quantor,
+		}
+	}
+
+	if coeff[from.Quantor] < coeff[to.Quantor] {
+		coeff1 := coeff[to.Quantor] / coeff[from.Quantor]
+		return checkerTimeQuantor{
+			Time:    to.Time/count + from.Time*coeff1,
+			Quantor: to.Quantor,
+		}
+	}
+
+	coeff2 := coeff[from.Quantor] / coeff[to.Quantor]
+	return checkerTimeQuantor{
+		Time:    coeff2*to.Time/count + from.Time,
+		Quantor: from.Quantor,
+	}
+}
+
+// checkerTransferQuantor - type for checkTransferQuantor
+type checkerTransferQuantor struct {
+	Transfer float64
+	Quantor  string
+}
+
+// checkTransferQuantor - check is quantor increase/decrease.
+func checkTransferQuantor(from, to checkerTransferQuantor) checkerTransferQuantor {
+	coeff := map[string]float64{
+		"b": 1000000,
+		"k": 1000,
+		"m": 1,
+	}
+	count := float64(config.Cfg.Try)
+	if from.Quantor == to.Quantor {
+		return checkerTransferQuantor{
+			Transfer: to.Transfer/count + from.Transfer,
+			Quantor:  to.Quantor,
+		}
+	}
+
+	if coeff[from.Quantor] < coeff[to.Quantor] {
+		coeff1 := coeff[to.Quantor] / coeff[from.Quantor]
+		return checkerTransferQuantor{
+			Transfer: to.Transfer/count + from.Transfer*coeff1,
+			Quantor:  to.Quantor,
+		}
+	}
+
+	coeff2 := coeff[from.Quantor] / coeff[to.Quantor]
+	return checkerTransferQuantor{
+		Transfer: coeff2*to.Transfer/count + from.Transfer,
+		Quantor:  from.Quantor,
+	}
+}
